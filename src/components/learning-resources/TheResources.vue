54ac511ee0b95554f736c9b8ec5386e95b9f1fee
Ahmed Helal Ahmed <ahmed.helal.online@gmail.com>
2021-09-25 00:39:52 +0200

    WIP

--- src/components/learning-resources/TheResources.vue
+++ src/components/learning-resources/TheResources.vue
@@ -18,8 +18,25 @@ export default {
   components: { StoredResources, AddResource },
   data() {
     return {
-      selectedTab: 'stored-resources'
+      selectedTab: 'stored-resources',
+      storedResources: [
+        {
+          id: 'official-guide',
+          title: 'Official Guide',
+          description: 'The official Vue.js documentation.',
+          link: 'https://vuejs.org'
+        },
+        {
+          id: 'google',
+          title: 'Google',
+          description: 'Learn to google...',
+          link: 'https://google.org'
+        }
+      ]
     };
+  },
+  provide() {
+    return { resources: this.storedResources };
   },
   methods: {
     setSelectedTab(tab) {
