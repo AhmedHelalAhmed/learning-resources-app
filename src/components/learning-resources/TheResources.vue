4bc87ef794ae515aa7ec67d8a4549c9c3c1c0018
Ahmed Helal Ahmed <ahmed.helal.online@gmail.com>
2021-09-25 01:18:45 +0200

    Finalize form the create resource

--- src/components/learning-resources/TheResources.vue
+++ src/components/learning-resources/TheResources.vue
@@ -11,7 +11,9 @@
       >Add Resource</base-button
     >
   </base-card>
-  <component :is="selectedTab"></component>
+  <keep-alive>
+    <component :is="selectedTab"></component>
+  </keep-alive>
 </template>
 
 <script>
@@ -48,11 +50,25 @@ export default {
     }
   },
   provide() {
-    return { resources: this.storedResources };
+    return {
+      resources: this.storedResources,
+      addResource: this.addResource
+    };
   },
   methods: {
     setSelectedTab(tab) {
       this.selectedTab = tab;
+    },
+    addResource(title, description, link) {
+      const newResource = {
+        id: new Date().toISOString(),
+        title: title,
+        description: description,
+        link: link
+      };
+      // this.storedResources.push(newResource);
+      this.storedResources.unshift(newResource);
+      this.selectedTab = 'stored-resources';
     }
   }
 };
